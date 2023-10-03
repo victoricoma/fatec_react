f1699e6261a0c2d5ae4cb6cd6b2344ab46007aff
VICTOR SENA ICOMA <victor.icoma@fatecmatao.local>
2023-10-03 12:33:38 -0300

    Aula0310 Componentes e Funções

--- react-basico/basic-app/src/App.jsx
+++ react-basico/basic-app/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import AppViewer from './components/AppViewer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
 <>
  <h1>Meu primeiro WebApp</h1>
+ <AppViewer />
 </>
   )
 }
--- react-basico/basic-app/src/components/AppViewer.jsx
+++ react-basico/basic-app/src/components/AppViewer.jsx
@@ -1,7 +1,14 @@
 import styles from './AppViewer.module.css'
+//Camada de Compilação de Serviços
+function somaNumero(n1, n2){
+  return(n1+n2)
+}
 const AppViewer = () => {
+  //Retorno no display
   return (
-    <div className={styles.avContainer}>AppViewer</div>
+    <>
+      <div className={somaNumero(2, 2) >10 ? console.log("Bunda"): console.log("Beiço")}>AppViewer</div>
+    </>
   )
 }
 
