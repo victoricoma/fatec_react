fda33715642152ae56e1dea93ba5bdbb991595e6
lemuelPires <lemuelpires@gmail.com>
2023-10-10 09:44:28 -0300

    atualizações

--- react-basico/basic-app/src/components/AppViewer.jsx
+++ react-basico/basic-app/src/components/AppViewer.jsx
@@ -1,7 +1,18 @@
 import styles from './AppViewer.module.css'
+//camada de compilação de serviços
+function somaNumero(n1,n2){
+    return (n1+n2)
+}
 const AppViewer = () => {
+    //retorno no display
   return (
-    <div className={styles.avContainer}>AppViewer</div> /*chamar css*/
+    <>
+    <div className={styles.avContainer}>AppViewer</div>
+    <p>
+        {somaNumero(5,17)}
+    </p>
+    </>
+    
   )
 }
 
